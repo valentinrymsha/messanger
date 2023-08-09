eecbc2934f4c4568f6214e78879547b735745ea0
Valentinrymsha <valentinrymsha@gmail.com>
2023-08-09 22:23:15 +0300

    Getting started

---
+++ Messanger/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Messanger
+//
+//  Created by Valentin on 9.08.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
