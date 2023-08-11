d45ffdc1bc9d9fb82902a186b7989887056e2d81
valentinrymsha <valentinrymsha@gmail.com>
2023-08-11 12:32:44 +0300

    added ui of login form

--- Messanger/Controllers/ConversationsViewController.swift
+++ Messanger/Controllers/ConversationsViewController.swift
@@ -11,9 +11,23 @@ class ConversationsViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        view.backgroundColor = #colorLiteral(red: 0.8549019694, green: 0.250980407, blue: 0.4784313738, alpha: 1)
     }
 
-
+    override func viewDidAppear(_ animated: Bool) {
+        super.viewDidAppear(animated)
+        
+        let isLogged = UserDefaults.standard.bool(forKey: "loggedIn ")
+        
+        if !isLogged {
+            let vc = LoginViewController()
+            let nav = UINavigationController(rootViewController: vc)
+            nav.modalPresentationStyle = .fullScreen 
+            nav.navigationBar.backgroundColor = .lightGray.withAlphaComponent(0.1)
+            present(nav, animated: false)
+        }
+    }
+    
+    
 }
 
--- Messanger/Controllers/Login&Register/LoginViewController.swift
+++ Messanger/Controllers/Login&Register/LoginViewController.swift
@@ -9,21 +9,118 @@ import UIKit
 
 class LoginViewController: UIViewController {
 
+    private let imageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.image = UIImage(named: "logo")
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    private let scrollView: UIScrollView = {
+        
+        let scrollView = UIScrollView()
+        scrollView.clipsToBounds = true
+        
+        return scrollView
+        
+    }()
+    
+    private let emailField: UITextField = {
+        let emailField = UITextField()
+        emailField.autocapitalizationType = .none
+        emailField.autocorrectionType = .no
+        emailField.returnKeyType = .continue
+        emailField.layer.cornerRadius = 13
+        emailField.layer.borderWidth = 1
+        emailField.layer.borderColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1).cgColor
+        emailField.placeholder = "Email Adress"
+        emailField.leftView = UIView(frame: CGRect(x: 0,
+                                     y: 0,
+                                     width: 5,
+                                     height: 0))
+        emailField.leftViewMode = .always
+        emailField.backgroundColor = .white
+        return emailField
+    }()
+    
+    private let passwordField: UITextField = {
+        let passwordField = UITextField()
+        passwordField.autocapitalizationType = .none
+        passwordField.autocorrectionType = .no
+        passwordField.returnKeyType = .continue
+        passwordField.layer.cornerRadius = 13
+        passwordField.layer.borderWidth = 1
+        passwordField.layer.borderColor = #colorLiteral(red: 0.501960814, green: 0.501960814, blue: 0.501960814, alpha: 1).cgColor
+        passwordField.placeholder = "Password"
+        passwordField.leftView = UIView(frame: CGRect(x: 0,
+                                     y: 0,
+                                     width: 5,
+                                     height: 0))
+        passwordField.isSecureTextEntry = true
+        passwordField.leftViewMode = .always
+        passwordField.backgroundColor = .white
+        return passwordField
+    }()
+    
+    private let loginButton: UIButton = {
+        let button = UIButton()
+        button.setTitle("Login", for: .normal)
+        button.backgroundColor = .link
+        button.setTitleColor(.white, for: .normal)
+        button.layer.cornerRadius = 13
+        button.layer.masksToBounds = true
+        button.titleLabel?.font = .systemFont(ofSize: 20,
+                                              weight: .bold)
+        
+        return button
+    }()
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-
-        // Do any additional setup after loading the view.
+        title = "Login"
+        view.backgroundColor = .white
+        
+        navigationItem.rightBarButtonItem = UIBarButtonItem(title: "Register",
+                                                            style: .done,
+                                                            target: self,
+                                                            action: #selector(didTapRegister))
+        view.addSubview(scrollView)
+        scrollView.addSubview(imageView)
+        scrollView.addSubview(emailField)
+        scrollView.addSubview(passwordField)
+        scrollView.addSubview(loginButton)
     }
     
-
-    /*
-    // MARK: - Navigation
-
-    // In a storyboard-based application, you will often want to do a little preparation before navigation
-    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
-        // Get the new view controller using segue.destination.
-        // Pass the selected object to the new view controller.
+    override func viewDidLayoutSubviews() {
+        super.viewDidLayoutSubviews()
+        
+        scrollView.frame = view.bounds
+        let size = scrollView.width / 5
+        
+        imageView.frame = CGRect(x: (view.width-size)/2,
+                                 y: 40,
+                                 width: size,
+                                 height: size)
+        
+        emailField.frame = CGRect(x: 30,
+                                  y: imageView.bot + 15,
+                                  width: scrollView.width - 60,
+                                  height: 52)
+        
+        passwordField.frame = CGRect(x: 30,
+                                     y: emailField.bot + 10,
+                                     width: scrollView.width - 60,
+                                     height: 52)
+        
+        loginButton.frame = CGRect(x: 30,
+                                   y: passwordField.bot + 15,
+                                   width: scrollView.width - 60,
+                                   height: 52)
     }
-    */
-
+    
+    @objc private func didTapRegister() {
+        let vc = RegisterViewController()
+        vc.title = "Create account"
+        navigationController?.pushViewController(vc, animated: true)
+    }
 }
